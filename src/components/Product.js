f3b3698856b4643db1e72a0ead4d95831fdb5efa
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 18:39:39 +0330

    add css for Store and Product files

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,15 +7,18 @@ import { CartContext } from '../context/CardContextProvider';
 //function
 import { isInCart, quantityCount, shorten } from '../helper/function';
 
+// Css
+import styles from './Product.module.css';
+
 const Product = ({itemData}) => {
 
     const {state, dispatch} = useContext(CartContext)
 
     return (
-        <div>
-            <img src={itemData.image} alt="product" style={{width:"300px"}} />
+        <div className={styles.Product}>
+            <img src={itemData.image} alt="product" />
             <h3>{shorten(itemData.title)}</h3>
-            <p>{itemData.price}</p>
+            <p>{itemData.price} $</p>
             <div>
                 <Link to={`/products/${itemData.id}`}>Details</Link>
                 <div>
--- src/components/Store.js
+++ src/components/Store.js
@@ -6,12 +6,15 @@ import { ProductsContext } from '../context/ProductsContextProvider';
 //Components
 import Product from './Product';
 
+// Css
+import styles from './Store.module.css'
+
 const Store = () => {
 
     const product = useContext(ProductsContext)
 
     return (
-        <div style={{display:"flex" , justifyContent:"space-between" , flexWrap:"wrap"}}>
+        <div className={styles.Store}>
             {
                 product.map( item => <Product key={item.id} itemData={item} />)
             }
