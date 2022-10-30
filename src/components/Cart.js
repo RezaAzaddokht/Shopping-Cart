92dfa3c89794863976e68d32d468f0637239ba3f
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 17:41:57 +0330

    complete cart

---
+++ src/components/Cart.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+
+// Context
+import { CartContext } from '../context/CardContextProvider';
+
+// Functions
+import { shorten } from '../helper/function';
+
+const Cart = (props) => {
+
+    const {dispatch} = useContext(CartContext);
+    const {image, title, price, quantity} = props.data;
+
+    return (
+        <div>
+            <img src={image} alt="prduct" />
+            <div>
+                <h3>{shorten(title)}</h3>
+                <p>{price} $</p>
+            </div>
+            <div>
+                <span>{quantity}</span>
+            </div>
+            <div>
+                {
+                    quantity > 1 ?
+                    <button onClick={() => dispatch({type: "DECREASE", payload: props.data})}>-</button> :
+                    <button onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})}>Remove</button>
+                }
+                <button onClick={() => dispatch({type: "INCREASE", payload: props.data})}>+</button>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
