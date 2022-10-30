432a9ea84ab04beacbb23a81de04678fc998b009
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 17:08:09 +0330

    add handlers in product file

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CardContextProvider';
 
 //function
-import { shorten } from '../helper/function';
+import { isInCart, quantityCount, shorten } from '../helper/function';
 
 const Product = ({itemData}) => {
 
@@ -19,7 +19,13 @@ const Product = ({itemData}) => {
             <div>
                 <Link to={`/products/${itemData.id}`}>Details</Link>
                 <div>
-                    <button>Add To Card</button>
+                    {quantityCount(state,itemData.id) > 1 && <button onClick={() => dispatch({type: "DECREASE", payload:itemData})}>-</button>}
+                    {quantityCount(state,itemData.id) === 1 && <button onClick={() => dispatch({type: "REMOVE_ITEM", payload:itemData})}>Remove</button>}
+                    {
+                        isInCart(state,itemData.id) ?
+                        <button onClick={() => dispatch({type:"INCREASE", payload:itemData})}>+</button> :
+                        <button onClick={() => dispatch({type: "ADD_ITEM", payload:itemData})}>Add to Cart</button>
+                    }
                 </div>
             </div>
         </div>
