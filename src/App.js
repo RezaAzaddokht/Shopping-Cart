5614cbd14ce22b49e35344b6be7d9dcecd3cf6eb
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 16:08:08 +0330

    add product detail page to router

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import ProductsContextProvider from './context/ProductsContextProvider';
 
 //Components
 import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
 
 
 function App() {
   return (
     <ProductsContextProvider>
       <Switch>
-        <Route path='/product' component={Store} />
-        <Redirect to='/product' />
+        <Route path='/products/:id' component={ProductDetails} />
+        <Route path='/products' component={Store} />
+        <Redirect to='/products' />
       </Switch>
     </ProductsContextProvider>
   );
