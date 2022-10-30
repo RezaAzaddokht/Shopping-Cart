e69becca8be4bc188a3c780c146a61e70ee700f2
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 18:15:14 +0330

    add css for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom';
 // Context
 import { CartContext } from '../context/CardContextProvider';
 
+// Css
+import styles from './Navbar.module.css'
+
 
 const Navbar = () => {
 
     const {state} = useContext(CartContext);
 
     return (
-        <div>
+        <div className={styles.navbar}>
+            <Link to="/products">Products</Link>
             <div>
-                <Link to="/products">Products</Link>
-                <div>
-                    <Link to="/Cart"><span>{state.itemsCounter}</span></Link>   
-                </div>
+                <Link to="/Cart"><span>{state.itemsCounter}</span></Link>   
             </div>
         </div>
     );
